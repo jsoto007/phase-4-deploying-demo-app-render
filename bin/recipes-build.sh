ea016087f52cb07a27acdd3f674674e97384e8ab
Jose Soto <jsoto7087@gmail.com>
2023-06-09 14:43:51 -0400

    update before render

--- bin/recipes-build.sh
+++ bin/recipes-build.sh
@@ -5,3 +5,10 @@ set -o errexit
 bundle install
 bundle exec rake db:migrate 
 bundle exec rake db:seed
+
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+bundle install
+bundle exec rake db:migrate
